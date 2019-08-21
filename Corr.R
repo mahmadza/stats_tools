00d0218f590a0dfc5fbfac395a88c6d5be8708cb
Muhammad Zabidi <maduh17@gmail.com>
2019-08-21 22:41:04 +0800

    Add files via upload

---
+++ Corr.R
@@ -0,0 +1,74 @@
+#!/biosw/debian7-x86_64/R/2.15.3/bin/Rscript --vanilla
+
+
+#paste0 function
+paste0 <- function( ..., sep="" ) paste( ..., sep = sep )
+
+#possible methods:
+#poss_method=c("pearson","spearman","kendall")
+
+#default argument
+test_method<-"spearman"
+
+#retrieve arguments
+args<-commandArgs(TRUE)
+
+#help
+help <- function(){
+    cat("\nCorr.R : Calculate correlation of a tab-delimited input using pearson, spearman or kendall method\n")
+    cat("\nUsage: Corr.R -i -\n")
+    cat("-i : input file or stdin (-)\n")
+    cat(paste0("-m : method (default: ",test_method,")\n\n"))
+    cat("CAUTION : Kendall can be very slow for really large dataset (>1,000)")
+    cat("\n\n")
+    q()
+}
+
+#save values of arguments
+if(length(args)==0 || !is.na(charmatch("-help",args))){
+    help()
+} else {
+    for(ii in 1:length(args)){
+	if(grepl("^-",args[ii]) && args[ii] != "-"){
+	    if(ii+1<=length(args) && (!grepl("^-",args[ii+1]) || args[ii+1]=="-")){
+	        assign(gsub("-","",args[ii]),args[ii+1])
+	    } else { assign(gsub("-","",args[ii]),1) }
+	}
+}}
+
+#assign test method
+test_method=m
+
+#load data into table
+if(exists("i")){
+    if(i=="-"){
+        d=as.matrix(read.table(pipe('cat /dev/stdin'))) # default is list so convert to matrix with as.matrix()
+    } else if (file.exists(i)){
+        d=as.matrix(read.table(i))
+    } else { cat("Input file does not exist\n"); q() }
+} else { cat("No input specified\n"); q() }
+
+
+#calculate correlation
+result=vector()
+result=cor(d[,'V1'],d[,'V2'],method=test_method)
+
+#Return result
+cat(format(result),"\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ student_paired.R
@@ -0,0 +1,45 @@
+#!/biosw/debian7-x86_64/R/3.0.2/bin/Rscript  --vanilla
+
+## Default arguments
+x=1 # x column
+y=2 # y column
+
+## Retrieve arguments
+args=commandArgs(TRUE)
+
+## Help
+help <- function(){
+    cat("\nstudent.R : Calculate the student's t-test p-value for two columns of a tab-delimited input\n")
+    cat("Usage: student.R -i -\n")
+    cat("-i : Input file or stdin (-)\n")
+    cat("-x : Which column is on the x axis (default=1)\n")
+    cat("-y : Which column is on the y axis (default=2)\n")
+    cat("\n")
+    q()
+}
+
+## Save values of each argument
+if(length(args)==0 || !is.na(charmatch("-help",args))){
+    help()
+} else {
+    for(ii in 1:length(args)){
+	if(grepl("^-",args[ii]) && args[ii] != "-"){
+	    if(ii+1<=length(args) && (!grepl("^-",args[ii+1]) || args[ii+1]=="-")){
+	        assign(gsub("-","",args[ii]),args[ii+1])
+	    } else { assign(gsub("-","",args[ii]),1) }
+	}
+}}
+
+## Load data into a table
+if(exists("i")){
+    if(i=="-"){
+        d=read.delim("/dev/stdin",header=F)
+    } else if (file.exists(i)){
+        d=read.delim(i,header=F)
+    } else { cat("Input file does not exist\n"); q() }
+} else { cat("No input specified\n"); q() }
+
+## Calculate student's t-test p-value
+student=signif(t.test(d[,as.numeric(x)],d[,as.numeric(y)],alternative=c("greater"),paired=TRUE)$p.value)
+cat(student,"\n")
+##default:one-sided x>y, unequal variance
